d9f2f10a272f56addbb349742c226825d886777e
Jack hosking <jackhosking2@outlook.com>
2019-01-22 19:04:05 +1300

    text on banner image, cards for display of app

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Logo from "./images/logo.png";
+import tiers from "./images/track_tiers.jpg";
+import data from "./images/track_data.jpg";
+import run from "./images/track_run.jpg";
 import "./css/home.css";
 import {
   Collapse,
@@ -59,58 +62,114 @@ export class Home extends Component {
             </Nav>
           </Collapse>
         </Navbar>
-
-        <div class="bg-img">
-          <div class="container-small">
-            <div class="card">
-              <h5 class="card-title text-center py-4">
-                <strong>Sign-up for Runfinity</strong>
-              </h5>
-
-              <div class="card-body px-lg-5">
-                <form class="text-center">
-                  <p class="orange-text">
-                    Closed-access beta, sign-ups have to be approved.
-                  </p>
-
-                  <div class="md-form mt-3">
-                    <input
-                      type="text"
-                      id="materialSubscriptionFormPasswords"
-                      class="form-control"
-                      placeholder="Name"
-                    />
+        <section>
+          <div class="bg-img">
+            <section>
+              <div class="container">
+                <div class="information">
+                  <div class="title">
+                    <strong class="motivation">TRACK EVERYTHING</strong>
+                    <p class="motivation-detail">
+                      Log any kind of workout using just your android phone.
+                    </p>
+                    <strong class="motivation">COMPETE WITH FRIENDS</strong>
+                    <p class="motivation-detail">
+                      Level up, and gain experience.
+                    </p>
+                    <strong class="motivation">TRAIN SMARTER</strong>
+                    <p class="motivation-detail">
+                      Analyze your data and conquer the new-year resolutions.
+                    </p>
                   </div>
+                </div>
+              </div>
+            </section>
+            <section>
+              <div class="sign-up">
+                <div class="container-small">
+                  <div class="card border-0">
+                    <h5 class="card-title text-center py-4">
+                      <strong>Sign-up for Runfinity</strong>
+                    </h5>
 
-                  <div class="md-form">
-                    <input
-                      type="email"
-                      id="materialSubscriptionFormEmail"
-                      class="form-control"
-                      placeholder="Email"
-                    />
-                    <small id="emailHelp" class="form-text text-muted">
-                      We'll never share your email with anyone else.
-                    </small>
+                    <div class="card-body px-lg-5">
+                      <form class="text-center">
+                        <p class="text-color-orange">
+                          Closed-access beta, sign-ups have to be approved.
+                        </p>
+
+                        <div class="md-form mt-3">
+                          <input
+                            type="text"
+                            id="materialSubscriptionFormPasswords"
+                            class="form-control"
+                            placeholder="Name"
+                          />
+                        </div>
+
+                        <div class="md-form">
+                          <input
+                            type="email"
+                            id="materialSubscriptionFormEmail"
+                            class="form-control"
+                            placeholder="Email"
+                          />
+                          <small
+                            id="emailHelp"
+                            class="form-text text-muted padding"
+                          >
+                            We'll never share your email.
+                          </small>
+                        </div>
+
+                        <button class="submit-btn" type="button">
+                          Let's get that bread
+                        </button>
+
+                        <p class="padding">
+                          <a href="" target="_blank">
+                            Already have access? click here
+                          </a>
+                        </p>
+                      </form>
+                    </div>
                   </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
 
-                  <button
-                    class="btn btn-outline-warning btn-rounded btn-block  my-4 waves-effect"
-                    type="button"
-                  >
-                    Request Access
-                  </button>
-
-                  <p>
-                    <a href="" target="_blank">
-                      Already have access? click here
-                    </a>
-                  </p>
-                </form>
+        <section>
+          <div class="row">
+            <div class="polaroid">
+              <img
+                class="card-header-image"
+                src={data}
+                alt="Track your run history"
+              />
+              <div class="card-body-orange">
+                <p>Track your run history</p>
+              </div>
+            </div>
+            <div class="polaroid">
+              <img class="card-header-image" src={run} alt="" />
+              <div class="card-body-orange">
+                <p>Keep coming back</p>
+              </div>
+            </div>
+            <div class="polaroid">
+              <img
+                class="card-header-image"
+                src={tiers}
+                alt="Easily view your progress"
+              />
+              <div class="card-body-orange">
+                <p>Enjoy your progress</p>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
     );
   }
