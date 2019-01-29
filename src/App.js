da9f6a009a07878b76e2ffd082c8b7c89d1fde8d
Jack hosking <jackhosking2@outlook.com>
2019-01-29 21:00:26 +1300

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import {Home} from "./views/Home";
+import { Home } from "./views/Home";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Runfinity">
         <Home />
       </div>
     );
---
+++ src/views/Footer.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./css/home.css";
+
+export class footer extends Component {
+  render() {
+    return <footer className="footer" />;
+  }
+}
